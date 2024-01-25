5d25f93de35e3b1f57c65dc5736a227bf1b51f82
SpizhovyiMaxDev <max.spizhovyi@gmail.com>
2024-01-25 00:40:32 -0800

    Updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react"
-import Button from "./components/Button"
-import Input from "./components/Input";
-import Logo from "./components/Logo";
-import NavBar from "./components/NavBar";
-import Main from "./components/Main"
-import useCrypto from "./hooks/useCrypto";
-import Container from "./components/Container";
-import CryptoList from "./components/CryptoList";
-import Footer from "./components/Footer";
-import SideBar from "./components/SideBar";
-import useToLocalStorageState from "./hooks/useSetToLocalStorageState"
-import Modal from "./components/Modal";
+import Button from "./Button"
+import Input from "./Input";
+import Logo from "./Logo";
+import NavBar from "./NavBar";
+import Main from "./Main"
+import useCrypto from "../hooks/useCrypto";
+import Container from "./Container";
+import CryptoList from "./CryptoList";
+import Footer from "./Footer";
+import SideBar from "./SideBar";
+import useToLocalStorageState from "../hooks/useSetToLocalStorageState"
+import Modal from "./Modal";
 
 function App() {
   const [ query, setQuery ] = useState("");
